adb3d522dffff520dcc04822430561a94d934a48
Camille Rabatel <crabatel@clever-age.com>
2020-11-10 16:08:07 +0100

    Add useFetch with cancellation

---
+++ src/examples/useFetch.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+
+function useFetch(url) {
+  const [result, setResult] = useState(undefined);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const controller = new AbortController();
+
+    fetch(url, { signal: controller.signal })
+      .then((response) => response.json())
+      .then(
+        (result) => {
+          setLoading(false);
+          setResult(result);
+          setError(null);
+        },
+        (error) => {
+          setLoading(false);
+
+          if (error.name !== "AbortError") {
+            setError(error);
+          }
+        }
+      );
+
+    return () => {
+      controller.abort();
+    };
+  }, [url]);
+
+  const _return = [result, loading, error];
+
+  _return.result = result;
+  _return.loading = loading;
+  _return.error = error;
+
+  return _return;
+}
+
+export default useFetch;
