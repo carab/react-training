b41a8ab1f0508bd8a11ee97693f862998a75a6df
Camille Rabatel <crabatel@clever-age.com>
2020-12-10 12:02:33 +0100

    Add PokemonList pagination

--- src/PokemonList/PokemonList.tsx
+++ src/PokemonList/PokemonList.tsx
@@ -1,39 +1,61 @@
+import { useState } from "react";
 import useFetch from "../hooks/useFetch";
 
 export type PokemonApiList = {
   count: number;
-  next: string|null;
-  previous: string|null;
+  next: string | null;
+  previous: string | null;
   results: Array<{
     name: string;
     url: string;
-  }>
+  }>;
+};
+
+const LIMIT = 20;
+
+function makeUrl(page: number) {
+  return `https://pokeapi.co/api/v2/pokemon?offset=${(page - 1) * LIMIT}&limit=${LIMIT}`;
 }
 
 function PokemonList() {
-  const [result, error, loading] = useFetch<PokemonApiList>('https://pokeapi.co/api/v2/pokemon')
+  const [page, setPage] = useState(1);
+  const [result, error, loading] = useFetch<PokemonApiList>(makeUrl(page));
+
+  const onNavigate = (diff: number) => {
+    setPage((page) => page + diff);
+  };
 
   if (error) {
     return <p>Error: {error.message}</p>;
   }
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (result) {
-    const { results } = result;
-
-    return (
-      <ul>
-        {results.map((pokemon) => (
-          <li key={pokemon.name}>{pokemon.name}</li>
-        ))}
-      </ul>
-    );
-  }
-
-  return null;
+  return (
+    <>
+      <button
+        disabled={!result?.previous}
+        type="button"
+        onClick={() => onNavigate(-1)}
+      >
+        Précédent
+      </button>
+      <span>Page: {page}</span>
+      <button
+        disabled={!result?.next}
+        type="button"
+        onClick={() => onNavigate(1)}
+      >
+        Suivant
+      </button>
+      {result ? (
+        <ul>
+          {result.results.map((pokemon) => (
+            <li key={pokemon.name}>{pokemon.name}</li>
+          ))}
+        </ul>
+      ) : null}
+      {loading ? <p>Loading...</p> : null}
+    </>
+  );
 }
 
 export default PokemonList;
--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,29 +1,40 @@
 import { useEffect, useState } from "react";
 
 export default function useFetch<R>(url: string) {
-    const [result, setResult] = useState<R|undefined>(undefined);
-    const [error, setError] = useState<Error|null>(null);
-    const [loading, setLoading] = useState(false);
-  
-    useEffect(() => {
-      setLoading(true);
-  
-      async function execute() {
-        try {
-          const response = await fetch(url);
-          const result = await response.json();
-  
-          setResult(result);
-          setError(null);
-        } catch (error) {
-          setError(error);
-        } finally {
-          setLoading(false);
-        }
-      }
-  
-      execute();
-    }, [url]);
+  const [result, setResult] = useState<R | undefined>(undefined);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState(false);
 
-    return [result, error, loading] as const
+  useEffect(function startRequest() {
+    setLoading(true);
+
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    async function execute() {
+      try {
+        const response = await fetch(url, {
+          signal
+        });
+        const result = await response.json();
+
+        setResult(result);
+        setError(null);
+      } catch (error) {
+        if (error.name !== 'AbortError') {
+          setError(error);
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    execute();
+
+    return function cancelRequest() {
+      controller.abort();
+    };
+  }, [url]);
+
+  return [result, error, loading] as const;
 }
