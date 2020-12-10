4447dc78418efef63db1a6dd2d22d0375ce66f26
Camille Rabatel <crabatel@clever-age.com>
2020-12-10 14:14:14 +0100

    Add simple routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
 import PokemonList from './PokemonList/PokemonList';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <PokemonList/>
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Accueil</Link>
+          {' '}
+          <Link to="/pokemon">Pokémon</Link>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <h1>Bonjour</h1>
+          </Route>
+
+          <Route path="/pokemon">
+            <PokemonList/>
+          </Route>
+
+          <Route>
+            <h1>Erreur 404</h1>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
