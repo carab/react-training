dd814834492d54f57944fdaaa57da126af023e97
Camille Rabatel <crabatel@clever-age.com>
2020-12-11 16:25:12 +0100

    Add portal example

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import {
@@ -9,7 +11,28 @@ function Navbar() {
   const { result } = useSelector(selectPokemonList);
   const checked = useSelector(selectPokemonListChecked);
 
-  return (
+  const [size, setSize] = useState<"small" | "medium" | null>(null);
+
+  useEffect(() => {
+    const onResize = () => {
+      const query = matchMedia("(max-width: 600px)");
+      if (query.matches) {
+        setSize("small");
+      } else {
+        setSize("medium");
+      }
+    };
+
+    onResize();
+
+    window.addEventListener("resize", onResize);
+
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
+
+  const root = (
     <nav>
       <Link to="/">Accueil</Link>{" "}
       <Link to="/pokemon">
@@ -17,6 +40,17 @@ function Navbar() {
       </Link>
     </nav>
   );
+
+  if (size === "small") {
+    return root;
+  }
+
+  const element = document.querySelector("#side");
+  if (size === "medium" && element) {
+    return ReactDOM.createPortal(root, element);
+  }
+
+  return null;
 }
 
 export default Navbar;
